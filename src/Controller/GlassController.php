253a636e8ef5cfa81ba61330b5f34192bdb6ff09
Jasmin0101 <zasminalave@gmail.com>
2025-01-31 09:11:02 +1000

    Symfony Forms begin

--- src/Controller/GlassController.php
+++ src/Controller/GlassController.php
@@ -2,36 +2,42 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Application;
 use App\Enums\ActionEnum;
 use App\Form\ApplicationType;
-use App\Form\DTO\CreateApplicationRequest;
 use App\Repository\ApplicationRepository;
+use App\Repository\StockRepository;
 use App\Repository\PortfolioRepository;
 use App\Repository\DepositaryRepository;
-use App\Repository\StockRepository;
-use App\Repository\UserRepository;
 use App\Service\DealService;
-use LDAP\Result;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Config\Security\AccessControlConfig;
-
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 class GlassController extends AbstractController
 {
+    private $stockRepository;
+    private $portfolioRepository;
+    private $applicationRepository;
+    private $dealService;
+
     public function __construct(
-        private readonly StockRepository $stockRepository,
-        private readonly UserRepository $userRepository,
-        private readonly ApplicationRepository $applicationRepository,
-        private readonly PortfolioRepository $portfolioRepository,
+        StockRepository $stockRepository,
+        PortfolioRepository $portfolioRepository,
+        ApplicationRepository $applicationRepository,
+        DealService $dealService,
         private readonly DepositaryRepository $depositoryRepository,
-        private readonly DealService $dealService
     ) {
 
+        $this->stockRepository = $stockRepository;
+        $this->portfolioRepository = $portfolioRepository;
+        $this->applicationRepository = $applicationRepository;
+        $this->dealService = $dealService;
     }
+
     #[Route('/glass/stock/{stockId}', name: 'app_stock_glass', methods: ['GET'])]
     public function getStockGlass(int $stockId): Response
     {
@@ -46,78 +52,57 @@ class GlassController extends AbstractController
             'SELL' => ActionEnum::SELL,
         ]);
     }
-    #[Route('/applications/create', name: 'app_create_application', methods: ['POST'])]
-    public function createApplication(Request $request): Response
+    #[Route('/applications/create', name: 'app_create_application', methods: ['GET', 'POST'])]
+    public function createApplication(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $userId = $this->getUser();
-        if (!$userId) {
-            return $this->json(['error' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
-        }
-
-
-        $quantity = $request->getPayload()->get('quantity');
-        $price = $request->getPayload()->get('price');
-        $stockId = $request->getPayload()->get('stock');
-        $action = ActionEnum::from($request->getPayload()->get('action'));
-        $portfolioId = $request->getPayload()->get('portfolio');
-
-        $portfolio = $this->portfolioRepository->find($portfolioId);
-        $stock = $this->stockRepository->find($stockId);
         $application = new Application();
-        $application->setQuantity($quantity);
-        $application->setAction($action);
-        $application->setPrice($price);
-        $application->setStock($stock);
-        $application->setPortfolio($portfolio);
+        $form = $this->createForm(ApplicationType::class, $application);
 
-        $this->applicationRepository->saveApplication($application);
+        $form->handleRequest($request);
 
-        $this->dealService->executeDeal($application->getId());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($application);
+            $entityManager->flush();
 
-        return new RedirectResponse('/applications/my/view');
+            // Execute deal after saving application
+            $this->dealService->executeDeal($application->getId());
 
-
-    }
-    #[Route('/applications/update/{applicationId}', name: 'app_update_application', methods: ['POST'])]
-    public function updateApplication(Request $request, int $applicationId)
-    {
-        $quantity = $request->getPayload()->get('quantity');
-        $price = $request->getPayload()->get('price');
-        $action = ActionEnum::from($request->getPayload()->get('action'));
-
-        $application = $this->applicationRepository->find($applicationId);
-
-        $application->setQuantity($quantity);
-        $application->setPrice($price);
-        $application->setAction($action);
-
-        $this->applicationRepository->saveApplication($application);
-
-
-        $this->dealService->executeDeal($applicationId);
-        // Используем редирект с правильной формой пути
-        return $this->redirectToRoute('app_update_application', ['applicationId' => $applicationId]);
-    }
-
-
-    #[Route('/applications/update/{applicationId}', name: 'app_update_show_application', methods: ['GET'])]
-
-    public function showUpdateApplication(int $applicationId)
-    {
-        $user = $this->getUser();
-        if ($user == null) {
-            return $this->json([
-                'message' => 'User not found',
-            ], Response::HTTP_NOT_FOUND);
+            return $this->redirectToRoute('app_stock_glass_view');
         }
 
-        $application = $this->applicationRepository->find($applicationId);
-        return $this->render('glass/stock_applications_update.html.twig', [
-            'application' => $application,
-            'BUY' => ActionEnum::BUY,
-            'SELL' => ActionEnum::SELL,
+        return $this->render('glass/application_create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/applications/update/{applicationId}', name: 'app_update_application', methods: ['GET', 'POST'])]
+    public function updateApplication(Request $request, int $applicationId, EntityManagerInterface $entityManager): Response
+    {
+        $application = $this->applicationRepository->find($applicationId);
+        if (!$application) {
+            throw $this->createNotFoundException('Application not found');
+        }
+
+        $form = $this->createForm(ApplicationType::class, $application);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            // Execute deal after updating application
+            $this->dealService->executeDeal($application->getId());
+
+            return $this->redirectToRoute('app_stock_glass_view');
+        }
+
+        return $this->render('glass/application_update.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
 
     #[Route('/application/delete/{applicationId}', name: 'app_stock_glass_delete_application', methods: ['POST'])]
 
--- src/Form/ApplicationType.php
+++ src/Form/ApplicationType.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Form;
+
 use App\Entity\Application;
 use App\Enums\ActionEnum;
-use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -11,30 +11,41 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class ApplicationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder
-            ->add('price', NumberType::class)
-            ->add('quantity', IntegerType::class)
-            ->add('user_id', IntegerType::class)
-            ->add('stock_id', IntegerType::class)
-            ->add(
-                'action',
-                EnumType::class,
-                ['class' => ActionEnum::class,]
-            );
+            ->add('price', NumberType::class, [
+                'label' => 'Price',
+                'required' => true,
+            ])
+            ->add('quantity', IntegerType::class, [
+                'label' => 'Quantity',
+                'required' => true,
+            ])
+            ->add('portfolio_id', IntegerType::class, [
+                'label' => 'Portfolio ID',
+                'required' => true,
+            ])
+            ->add('stock_id', IntegerType::class, [
+                'label' => 'Stock ID',
+                'required' => true,
+            ])
+            ->add('action', EnumType::class, [
+                'class' => ActionEnum::class,
+                'choice_label' => function ($action) {
+                    return $action->getLabel(); // Assuming you have a method to get the label
+                },
+                'label' => 'Action',
+                'required' => true,
+            ]);
     }
 
-    public function configurationOptions(OptionsResolver $resliver)
+    public function configureOptions(OptionsResolver $resolver)
     {
-        $resliver->setDefaults([
-
-            'data_class' => Application::class
-
+        $resolver->setDefaults([
+            'data_class' => Application::class,
         ]);
     }
 }
